ea2eb96bf998bb5bd9d32845b8d65002d7dc9a5d
LightningStrike83 <113542893+LightningStrike83@users.noreply.github.com>
2023-08-02 22:42:45 -0400

    Skeleton of Format
    Just coping and pasting the formatting~

---
+++ js/questions.js
@@ -0,0 +1,81 @@
+let questions = [
+    {
+        question : "When is Soleil's birthday?",
+        answer1 : "August 7th",
+        answer2 : "August 31st",
+        answer3 : "August 20th",
+        answer4 : "August 16th"
+    },
+
+    {
+        question : "What is Leo's favourite food?",
+        answer1 : "Tomatoes",
+        answer2 : "Pickles",
+        answer3 : "Bear meat",
+        answer4 : "Beef Stew",
+    },
+
+    {
+        question : "Which character does Chris Hackney not voice?",
+        answer1 : "Dimitri",
+        answer2 : "Boey",
+        answer3 : "Quan",
+        answer4 : "Seth",
+    },
+
+    {
+        question : "What is Kagero's secondary hobby?",
+        answer1 : "Woodcarving",
+        answer2 : "Mechanics",
+        answer3 : "Archery",
+        answer4 : "Pottery",
+    },
+
+    {
+        question : "Which of these heroes has not been drawn by Wada Sachiko?",
+        answer1 : "Titania",
+        answer2 : "Sigurd",
+        answer3 : "Mark",
+        answer4 : "Shinon",
+    },
+
+    {
+        question : "Which continent does not have wyverns?",
+        answer1 : "Elibe",
+        answer2 : "Fateslandia",
+        answer3 : "Valentia",
+        answer4 : "Elyos",
+    },
+
+    {
+        question : "Which of the Whitewings appeared as a mirage in Tokyo Mirage Sessions #FE?",
+        answer1 : "Palla",
+        answer2 : "Catria",
+        answer3 : "Est",
+        answer4 : "Minerva",
+    },
+
+    {
+        question : "What is the only other child of Chrom that reveals the location of their brand?",
+        answer1 : "Morgan",
+        answer2 : "Kjelle",
+        answer3 : "Cynthia",
+        answer4 : "Inigo",
+    },
+
+    {
+        question : "Which hero does this set of skills belong to? Iceberg, Special Spiral 4, Atk/Res Oath 4.",
+        answer1 : "Arval- Cycle Keeper",
+        answer2 : "Citrinne- Caring Noble",
+        answer3 : "Ophelia- Starlit Maiden",
+        answer4 : "Celine- Joyous Royal",
+    },
+
+    {
+        question : "What is the only route in Fire Emblem: Three Houses where it's possible to get same-gender endings for all of the non-recruited units?",
+        answer1 : "Crimson Flower",
+        answer2 : "Silver Snow",
+        answer3 : "Azure Moon",
+        answer4 : "Verdent Wind",
+    },
+]
